5b161b9183997bba299f0a94becf8512f0e33c77
DIPAK AHER <aherdipak3@gmail.com>
2019-05-05 15:36:18 +0530

    Exception Handling in springMVC

---
+++ SpringMVC_ExceptionHandling/src/main/java/com/dac/controller/AdmissionController.java
@@ -0,0 +1,59 @@
+package com.dac.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdmissionController {
+	
+	@RequestMapping(value="/getStudentAdmissionFormPage.htm", method=RequestMethod.GET)
+	public ModelAndView getStudentAdmissionFormPage() throws IOException {
+		
+		String exceptionOccured = "NULL_POINTER";
+		if(exceptionOccured.equalsIgnoreCase("NULL_POINTER")) {
+//			throw new NullPointerException("Null Pointer Exception");
+//			throw new IOException("IO Exception");
+			throw new ArithmeticException("Arithmetic Exception");
+		}
+		
+		ModelAndView modelAndView = new ModelAndView("studentAdmissionForm");
+		modelAndView.addObject("headerMsg", "Enginnering College,India");
+		return modelAndView;
+	}
+	 
+	
+	@ExceptionHandler(value = NullPointerException.class)
+	public String handleNullPointerException(Exception e){
+
+	// logging Null Pointer Exception
+	System.out.println("Null Pointer Exception Occured: "+e);
+
+	return "NullPointerException";
+
+	}
+	
+	@ExceptionHandler(value = IOException.class)
+	public String handleIoException(Exception e){
+
+	System.out.println("IO Exception Occured: "+e);
+
+	return "IOException";
+
+	}
+	
+	
+	@ExceptionHandler(value = Exception.class)
+	public String handleException(Exception e){
+
+	System.out.println("IO unknown Occured: "+e);
+
+	return "Exception";
+
+	}
+
+}
---
+++ SpringMVC_ExceptionHandling/src/main/webapp/WEB-INF/views/Exception.jsp
@@ -0,0 +1,15 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<html>
+<head>
+<title>success</title>
+</head>
+<body>
+
+	<h1>${headerMsg}</h1>
+	<h1>STUDENT ADMISSION FORM FOR ENGINEERING COURSE</h1>
+	
+	<p>The application has encountered a unknown error. Please contact support by sending an email at webmaster@dac.com</p>
+
+</body>
+</html>
