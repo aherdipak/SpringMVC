1d9c692f36e23dce378ba770013e8bc3c67fffac
DIPAK AHER <aherdipak3@gmail.com>
2019-06-07 00:31:50 +0530

    JSON Responce rename,ordring,excluding

--- SpringMVC_WebServices/src/main/java/com/dac/web/beans/Student.java
+++ SpringMVC_WebServices/src/main/java/com/dac/web/beans/Student.java
@@ -1,10 +1,31 @@
 package com.dac.web.beans;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({"address"})
+@JsonPropertyOrder({"hobby","student_Name","address"})
 public class Student {
 
+	@JsonProperty("student_Name")
 	private String name;
+	
 	private String address;
 	
+	private String hobby;
+	
+	
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
 	public String getName() {
 		return name;
 	}
