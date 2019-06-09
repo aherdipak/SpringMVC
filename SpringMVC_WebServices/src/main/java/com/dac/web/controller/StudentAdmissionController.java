8758439c61b7f39aed41465575fa3732f3972f3f
DIPAK AHER <aherdipak3@gmail.com>
2019-06-09 19:18:44 +0530

    getting single object using REST API

--- SpringMVC_WebServices/src/main/java/com/dac/web/controller/StudentAdmissionController.java
+++ SpringMVC_WebServices/src/main/java/com/dac/web/controller/StudentAdmissionController.java
@@ -3,7 +3,10 @@ package com.dac.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,8 @@ public class StudentAdmissionController {
 		ModelAndView model = new ModelAndView("admissionForm");
 		return model;
 	}
+	
+	/************* Retrieving All students records ****************************/
 	
 	 @ResponseBody // to convert java object to JSON String 
 	 @RequestMapping(value= "/student", method=RequestMethod.GET)
@@ -36,4 +41,19 @@ public class StudentAdmissionController {
 	   list.add(std4);
 	   return list;
 	 }
+	 
+	 //************* Retrieving  student record ****************************
+	 @ResponseBody  
+	 @RequestMapping(value= "/student/{name}", method=RequestMethod.GET)
+	 public Student getStudent(@PathVariable("name") String studentName) {
+		 
+		 // fetch the student's record using studentName from the DB
+		 Student student = new Student();
+		 student.setName(studentName);
+		 student.setHobby("WWE");
+		 System.out.println(studentName);
+		 return student;
+	 }
+	 
+	 
 }
